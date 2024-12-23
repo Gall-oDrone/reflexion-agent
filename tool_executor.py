76c49b347bcb538eafd9d897e54ef7d0ced3c110
Gall-oDrone <piehavok@hotmail.com>
2024-12-22 18:25:20 -0600

    Tool Executor Agent (Part B)

--- tool_executor.py
+++ tool_executor.py
@@ -1,12 +1,35 @@
 from typing import List
 from dotenv import load_dotenv
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_community.utilities.tavily_search import TavilySearchAPIWrapper
 from langchain_core.messages import BaseMessage, ToolMessage, HumanMessage, AIMessage
+from langgraph.prebuilt import ToolInvocation, ToolExecutor
+from chains import parser
 from schemas import AnswerQuestion
 
 load_dotenv()
 
+search = TavilySearchAPIWrapper()
+tavily_tool = TavilySearchResults(api_wrapper=search, max_results=5)
+tool_executor = ToolExecutor([tavily_tool])
+
+
 def execute_tools(state: List[BaseMessage]) -> List[ToolMessage]:
     tool_invocation: AIMessage = state[-1]
+    parsed_tool_calls = parser.invoke(tool_invocation)
+
+    ids = []
+    tool_invocations = []
+
+    for parsed_call in parsed_tool_calls:
+        for query in parsed_call["args"]["search_queries"]:
+            tool_invocation.append(ToolInvocation(
+                tool = "tavily_search_results_json",
+                tool_input=query,
+            ))
+            ids.append(parsed_call["id"])
+
+    outputs = tool_executor.batch(tool_invocations)
     pass
 
 if __name__ == "__main__":

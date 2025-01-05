46844034de88fed09e2cdbe55be230cf1cb0cf0d
Gall-oDrone <piehavok@hotmail.com>
2025-01-04 19:24:46 -0600

    Building our LangGraph Graph

--- main.py
+++ main.py
@@ -8,46 +8,38 @@ from langchain_core.output_parsers.openai_tools import (
     JsonOutputToolsParser,
     PydanticToolsParser,
 )
-from langchain_core.messages import HumanMessage
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_openai import ChatOpenAI
+from langchain_core.messages import BaseMessage, HumanMessage, ToolMessage
 from langgraph.graph import END, MessageGraph
 
-from chains import generate_chain, reflect_chain
+from chains import revisor, first_responder
+from tool_executor import execute_tools
 
-REFLECT = "reflect"
-GENERATE = "generate"
-
-def generation_node(state: Sequence[BaseMessage]):
-    return generate_chain.invoke({"messages":state})
-
-def reflection_node(messages: Sequence[BaseMessage]) -> List[BaseMessage]:
-    res = reflect_chain.invoke({"messages": messages})
-    return [HumanMessage(content=res.content)]
-
+MAX_ITERATIONS = 2
 builder = MessageGraph()
-builder.add_node(GENERATE, generation_node)
-builder.add_node(REFLECT, reflection_node)
-builder.set_entry_point(GENERATE)
+builder.add_node("draft", first_responder)
+builder.add_node("execute_tools", execute_tools)
+builder.add_node("revise", revisor)
+builder.add_edge("draft", "execute_tools")
+builder.add_edge("execute_tools", "revise")
 
-def should_continue(state: List[BaseMessage]):
-    if len(state) > 6:
+def event_loop(state: List[BaseMessage]) -> str:
+    count_tool_visits = sum(isinstance(item, ToolMessage) for item in state)
+    num_iterations = count_tool_visits
+    if num_iterations > MAX_ITERATIONS:
         return END
-    return REFLECT
+    return "execute_tools"
 
-builder.add_conditional_edges(GENERATE, should_continue)
-builder.add_edge(REFLECT, GENERATE)
-
+builder.add_conditional_edges("revise", event_loop)
+builder.set_entry_point("draft")
 graph = builder.compile()
-print(graph.get_graph().draw_mermaid())
-graph.get_graph().print_ascii()
+
+print(graph.get_graph().draw_ascii())
+graph.get_graph().draw_mermaid_png(output_file_path="graph.png")
 
 if __name__ == "__main__":
     print("Hello Reflexion Agent")
-    inputs = HumanMessage(content="""Make this tweet better:
-                                    @LangChainAI
-            - newly Tool Calling feature is seriously underrated,
-            After a long wait, it's here- making the implementation of agents across different models with function calls
-            Make a video covering their newest blog post
-    """)
-    response = graph.invoke(inputs)
+    res = graph.invoke(
+        "Write about AI-Powered / autonomous soc problem domain, list startups that do that and raised capital."
+    )
+    print(res[-1].tool_calls[0]["args"]["answer"])
+    print(res)
